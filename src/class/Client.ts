5729f6bb8337c6631c74268227cad20496ab1e0d
uSebazz <90474850+uSebazz@users.noreply.github.com>
2022-03-11 11:53:00 -0500

    arreglado

--- src/class/Client.ts
+++ src/class/Client.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+import 'dotenv/config';
 import '@sapphire/plugin-i18next/register';
 import mongoose from 'mongoose';
 import colors from 'colors';
@@ -8,142 +8,70 @@ import {
 	RegisterBehavior,
 	LogLevel,
 } from '@sapphire/framework';
-import type {
-	TextChannel,
-	ThreadChannel,
-	NewsChannel,
-} from 'discord.js';
+import type { TextChannel, ThreadChannel, NewsChannel } from 'discord.js';
 import { InternationalizationContext } from '@sapphire/plugin-i18next';
-import {
-	Model,
-	defaultData,
-} from '../lib/database/guildConfig';
+import { Model, defaultData } from '../lib/database/guildConfig';
 import { NinoMusic } from './Music';
 import { load } from '@lavaclient/spotify';
 import { Queue } from '../lib/function/queue';
 
-mongoose
-	.connect(
-		process
-			.env
-			.mongourl
-	)
-	.then(
-		() => {
-			console.log(
-				colors.blue(
-					`${new Date().toLocaleString()}`
-				),
-				'| Mongoose Connected'
-			);
-		}
-	);
+mongoose.connect(process.env.mongourl).then(() => {
+	console.log(colors.blue(`${new Date().toLocaleString()}`), '| Mongoose Connected');
+});
 
-load(
-	{
-		client: {
-			id: process
-				.env
-				.id,
-			secret: process
-				.env
-				.secret,
-		},
-		autoResolveYoutubeTracks:
-			true,
-	}
-);
+load({
+	client: {
+		id: process.env.id,
+		secret: process.env.secret,
+	},
+	autoResolveYoutubeTracks: true,
+});
 
 export class Nino extends SapphireClient {
 	readonly music: NinoMusic;
 	public constructor() {
-		super(
-			{
-				intents: 16071,
-				partials: [
-					'MESSAGE',
-					'CHANNEL',
-					'USER',
-					'GUILD_MEMBER',
-					'GUILD_SCHEDULED_EVENT',
-					'REACTION',
-				],
-				presence: {
-					status: 'idle',
-					activities:
-						[
-							{
-								name: '🌸 inv.nino.fun | dc.nino.fun',
-								type: 'WATCHING',
-							},
-						],
-				},
-				i18n: {
-					fetchLanguage:
-						async (
-							context: InternationalizationContext
-						) => {
-							if (
-								!context.guild
-							)
-								return 'en-US';
-
-							let guild =
-								await Model.findOne(
-									{
-										guild: context
-											.guild
-											.id,
-									}
-								).lean();
-							if (
-								!guild
-							)
-								guild =
-									await Model.create(
-										defaultData(
-											context
-												.guild
-												.id
-										)
-									);
-							return guild
-								.config
-								.language;
-						},
-				},
-				retryLimit: 2,
-				allowedMentions:
+		super({
+			intents: 16071,
+			partials: [
+				'MESSAGE',
+				'CHANNEL',
+				'USER',
+				'GUILD_MEMBER',
+				'GUILD_SCHEDULED_EVENT',
+				'REACTION',
+			],
+			presence: {
+				status: 'idle',
+				activities: [
 					{
-						repliedUser:
-							false,
+						name: '🌸 inv.nino.fun | dc.nino.fun',
+						type: 'WATCHING',
 					},
-				defaultPrefix:
-					'n/',
-			}
-		);
-		this.music =
-			new NinoMusic();
+				],
+			},
+			i18n: {
+				fetchLanguage: async (context: InternationalizationContext) => {
+					if (!context.guild) return 'en-US';
+
+					let guild = await Model.findOne({ guild: context.guild.id }).lean();
+					if (!guild) guild = await Model.create(defaultData(context.guild.id));
+					return guild.config.language;
+				},
+			},
+			retryLimit: 2,
+			allowedMentions: { repliedUser: false },
+			defaultPrefix: 'n/',
+		});
+		this.music = new NinoMusic();
 	}
-	async start(
-		token: string
-	) {
-		ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(
-			RegisterBehavior.Overwrite
-		);
+	async start(token: string) {
+		ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.Overwrite);
 		//this.loadMusic();
-		await super.login(
-			token
-		);
+		await super.login(token);
 	}
 }
 
-export type MessageChannel =
-
-		| TextChannel
-		| ThreadChannel
-		| NewsChannel
-		| null;
+export type MessageChannel = TextChannel | ThreadChannel | NewsChannel | null;
 
 declare module '@sapphire/framework' {
 	export interface SapphireClient {
