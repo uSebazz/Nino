f864100391cb487f3f312c076c6858eda1b544e3
uSebazz <cuentaparaelpaisagod@gmail.com>
2022-04-22 17:01:07 +0000

    feat(music): No support for music because discord terms

--- src/listeners/music/disconnect.ts
+++
@@ -1,9 +0,0 @@
-import { container, Listener } from '@sapphire/framework'
-import { ApplyOptions } from '@sapphire/decorators'
-
-@ApplyOptions<Listener.Options>({ event: 'disconnect', emitter: container.client.music })
-export class disconnectListener extends Listener {
-	public run(reason: string): void {
-		container.logger.debug(`Disconnected from Lavalink: ${reason}`)
-	}
-}
--- src/listeners/music/error.ts
+++
@@ -1,9 +0,0 @@
-import { container, Listener } from '@sapphire/framework'
-import { ApplyOptions } from '@sapphire/decorators'
-
-@ApplyOptions<Listener.Options>({ event: 'error', emitter: container.client.music })
-export class errorListener extends Listener {
-	public run(error: Error): void {
-		container.logger.error(error)
-	}
-}
