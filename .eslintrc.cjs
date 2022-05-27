b244296a77470d5f0be9c981cf47672a6a2b219a
Sebazz <90474850+uSebazz@users.noreply.github.com>
2022-05-27 10:26:23 -0500

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,79 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    '@typescript-eslint'
-  ],
-  rules: {
-    strict: 'error',
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
-    indent: ['error', 2],
-    'space-before-function-paren': ['error', 'never'],
-    'no-use-before-define': ['warn', { functions: true, classes: true, variables: true }],
-    'no-tabs': ['error', { allowIndentationTabs: false }],
-    'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-    'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
-    'non-unused-expressions': ['off'],
-    'linebreak-style': ['error', 'unix']
-  }
+	env: {
+		'es2021': true,
+		'node': true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/array-type': [
+			'error',
+			{
+				'default': 'array-simple',
+				'readonly': 'array-simple'
+			}
+		],
+		'@typescript-eslint/await-thenable': 'error',
+		'@typescript-eslint/consistent-type-assertions': 'error',
+		'@typescript-eslint/consistent-type-definitions': 'error',
+		'@typescript-eslint/consistent-type-imports': 'error',
+		'@typescript-eslint/explicit-member-accessibility': 'error',
+		'@typescript-eslint/no-base-to-string': 'error',
+		'@typescript-eslint/no-namespace': 'off',
+		'@typescript-eslint/no-confusing-non-null-assertion': 'error',
+		'@typescript-eslint/no-dynamic-delete': 'error',
+		'@typescript-eslint/no-floating-promises': 'error',
+		'@typescript-eslint/no-unnecessary-condition': 'error',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/prefer-includes': 'error',
+		'@typescript-eslint/prefer-optional-chain': 'error',
+		'@typescript-eslint/prefer-return-this-type': 'error',
+		'@typescript-eslint/require-await': 'off',
+		'arrow-body-style': 'error',
+		'arrow-spacing': 'error',
+		'block-spacing': 'error',
+		'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+		'comma-spacing': 'error',
+		'consistent-return': 'error',
+		'curly': ['error', 'multi-line'],
+		'dot-location': ['error', 'property'],
+		'dot-notation': 'error',
+		'eol-last': ['error', 'always'],
+		'eqeqeq': ['error', 'always'],
+		'key-spacing': 'error',
+		'keyword-spacing': 'error',
+		'max-classes-per-file': ['error', 1],
+		'new-parens': ['error', 'always'],
+		'newline-per-chained-call': 'error',
+		'no-constructor-return': 'error',
+		'no-multiple-empty-lines': 'error',
+		'no-nested-ternary': 'error',
+		'no-return-await': 'error',
+		'no-template-curly-in-string': 'error',
+		'no-trailing-spaces': 'error',
+		'no-unsafe-optional-chaining': 'error',
+		'no-var': 'error',
+		'object-curly-spacing': ['error', 'always'],
+		'prefer-arrow-callback': 'error',
+		'prefer-const': 'error',
+		'prefer-destructuring': 'error',
+		'prefer-rest-params': 'error',
+		'prefer-spread': 'error',
+		'prefer-template': 'error',
+		'quotes': ['error', 'single'],
+		'radix': ['error', 'always'],
+		'require-await': 'error',
+		'semi': ['error', 'never']
+	}
 }
