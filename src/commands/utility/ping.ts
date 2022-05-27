2a1df7e861a1c0839276a8d708767f9a09f0f773
uSebazz <cuentaparaelpaisagod@gmail.com>
2022-05-27 11:00:20 -0500

    ...

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -6,57 +6,40 @@ import { send } from '@sapphire/plugin-editable-commands'
 import { Emojis } from '#utils/constans'
 
 @ApplyOptions<NinoCommandOptions>({
-<<<<<<< HEAD
 	description: 'Ping of the bot',
 	aliases: ['pong', 'latency'],
 	chatInputCommand: {
 		register: true,
 		idHints: ['974700576971587584']
 	}
-=======
-  description: 'Ping of the bot',
-  aliases: ['pong', 'latency'],
-  chatInputCommand: {
-    register: true,
-    idHints: ['974700576971587584']
-  }
->>>>>>> 07ac729a5f0dc7382fc399bd83c9700207a0ae99
 })
 
 export class UserCommand extends NinoCommand {
-  public override async messageRun(message: Message): Promise<void> {
-    const msg = await send(message, `${Emojis.ninozzz} ping?`)
-    const diff = msg.createdTimestamp - message.createdTimestamp
-    const ping = Math.round(this.container.client.ws.ping)
+	public override async messageRun(message: Message): Promise<void> {
+		const msg = await send(message, `${Emojis.ninozzz} ping?`)
+		const diff = msg.createdTimestamp - message.createdTimestamp
+		const ping = Math.round(this.container.client.ws.ping)
 
-    await send(message, await resolveKey(message, 'commands/util:ping', { diff, ping }))
-  }
+		await send(message, await resolveKey(message, 'commands/util:ping', { diff, ping }))
+	}
 
-<<<<<<< HEAD
 	public override async chatInputRun(interaction: CommandInteraction): Promise<void> {
 		const msg = await interaction.reply({
 			content: `${Emojis.ninozzz} ping?`,
 			fetchReply: true
 		})
-=======
-  public override async chatInputRun(interaction: CommandInteraction): Promise<void> {
-    const msg = await interaction.reply({
-      content: `${Emojis.ninozzz} ping?`,
-      fetchReply: true
-    })
->>>>>>> 07ac729a5f0dc7382fc399bd83c9700207a0ae99
 
-    if (msg instanceof Message) {
-      const { diff, ping } = this.getPing(msg, interaction)
+		if (msg instanceof Message) {
+			const { diff, ping } = this.getPing(msg, interaction)
 
-      await msg.edit(await resolveKey(interaction, 'commands/util:ping', { diff, ping }))
-    }
-  }
+			await msg.edit(await resolveKey(interaction, 'commands/util:ping', { diff, ping }))
+		}
+	}
 
-  private getPing(message: Message, interaction: CommandInteraction) {
-    const diff = (message.editedTimestamp || message.createdTimestamp) - interaction.createdTimestamp
-    const ping = Math.round(this.container.client.ws.ping)
+	private getPing(message: Message, interaction: CommandInteraction) {
+		const diff = (message.editedTimestamp || message.createdTimestamp) - interaction.createdTimestamp
+		const ping = Math.round(this.container.client.ws.ping)
 
-    return { diff, ping }
-  }
+		return { diff, ping }
+	}
 }
