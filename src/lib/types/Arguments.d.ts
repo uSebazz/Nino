86391039fae411aec18e3a67383d9b87ba4366c9
ChikoShidori <degnan200@gmail.com>
2022-07-05 01:06:39 +0100

    refactor: the types were moved to another file

---
+++ src/lib/types/Arguments.d.ts
@@ -0,0 +1,31 @@
+// declare things
+import type { PrismaClient } from '@prisma/client'
+import type { ArrayString } from '@skyra/env-utilities'
+import type { User } from 'discord.js'
+
+declare module '@sapphire/framework' {
+	interface Preconditions {
+		DevOnly: never
+		Administrator: never
+	}
+
+	interface ArgType {
+		resolveUser: User
+	}
+}
+
+declare module '@sapphire/pieces' {
+	interface Container {
+		prisma: PrismaClient
+	}
+}
+
+declare module '@skyra/env-utilities' {
+	interface Env {
+		DISCORD_TOKEN: string
+		WEBHOOK_TOKEN: string
+		STATCORD_TOKEN: string
+		CLIENT_ID: string
+		CLIENT_OWNERS: ArrayString
+	}
+}
