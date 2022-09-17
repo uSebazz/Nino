cfd078733dde5cbf87ac13c1a3c62db136244666
diabla <95551108+diiabla@users.noreply.github.com>
2022-09-18 00:29:04 +0200

    refactor: remove unused interfaces

--- src/lib/types/Discord.ts
+++ src/lib/types/Discord.ts
@@ -1,14 +1,8 @@
 import type { GuildTextBasedChannelTypes } from '@sapphire/discord.js-utilities';
-import type { Guild, GuildMember, Message, DMChannel } from 'discord.js';
+import type { Guild, GuildMember, Message } from 'discord.js';
 
 export interface GuildMessage extends Message {
 	channel: GuildTextBasedChannelTypes;
 	readonly guild: Guild;
 	readonly member: GuildMember;
-}
-
-export interface DMMessage extends Message {
-	channel: DMChannel;
-	readonly guild: null;
-	readonly member: null;
 }
--- src/lib/utils/function/check.ts
+++ src/lib/utils/function/check.ts
@@ -1,10 +1,6 @@
-import type { DMMessage, GuildMessage } from '#lib/types';
+import type { GuildMessage } from '#lib/types';
 import type { Message } from 'discord.js';
 
 export function isGuildMessage(message: Message): message is GuildMessage {
 	return message.guild !== null;
-}
-
-export function isDmMessage(message: Message): message is DMMessage {
-	return message.guild === null;
 }
