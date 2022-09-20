8552a0b57f641575a9fcd756b0876bbc6fdf0a85
ChikoShidori <degnan200@gmail.com>
2022-09-20 09:01:28 +0100

    feat(db): timestamps

--- src/lib/utils/sanitizer/initClean.ts
+++ src/lib/utils/sanitizer/initClean.ts
@@ -2,7 +2,7 @@ import { initClean } from '#utils/sanitizer';
 import { isNullishOrEmpty } from '@sapphire/utilities';
 
 const secrets = new Set<string>();
-const suffixes = ['_KEY', '_TOKEN', '_SECRET', '_PASSWORD'];
+const suffixes = ['_KEY', '_TOKEN', '_SECRET', '_PASSWORD', '_URL'];
 for (const [key, value] of Object.entries(process.env)) {
 	if (isNullishOrEmpty(value)) continue;
 	if (suffixes.some((suffix) => key.endsWith(suffix))) secrets.add(value);
